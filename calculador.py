ce53f61df0ec0bf9d98289ae8a48679c8404c063
luciacantos <luciacantos22@gmail.com>
2024-02-11 22:39:52 +0100

    dibujo de las áreas

--- calculador.py
+++ calculador.py
@@ -24,11 +24,29 @@ def calcular_area_disponible():
         tiempo = area_disponible / velocidad_media
         tiempo_minutos = tiempo / 60
 
-
         dibujar_areas(largo_habitacion, ancho_habitacion, largo_area_no_utilizable, ancho_area_no_utilizable)
 
     except ValueError as e:
         resultado_area_disponible.config(text=f"Error: {e}")
+
+
+def dibujar_areas(largo_habitacion, ancho_habitacion, largo_area_no_utilizable, ancho_area_no_utilizable):
+
+    x_no_utilizable = (ancho_habitacion - ancho_area_no_utilizable) / 2
+    y_no_utilizable = (largo_habitacion - largo_area_no_utilizable) / 2
+
+    plt.figure()
+    plt.gca().add_patch(plt.Rectangle((0, 0), ancho_habitacion, largo_habitacion, color='blue', alpha=0.5))
+    plt.gca().add_patch(plt.Rectangle((x_no_utilizable, y_no_utilizable), ancho_area_no_utilizable, largo_area_no_utilizable, color='red', alpha=0.5))
+
+
+    plt.xlim(0, ancho_habitacion)
+    plt.ylim(0, largo_habitacion)
+
+
+    plt.grid(True)
+    plt.title('Áreas habitación y mueble')
+
 
 
 
